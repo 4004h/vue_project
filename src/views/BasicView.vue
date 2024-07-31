d5c77e59e700871b41a45dc82b5dcca770313269
4004h <minjihwan9500@gmail.com>
2024-07-31 11:19:09 +0900

    라우터, 컴포넌트

---
+++ src/views/BasicView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="Basic">
+    <h1>hellow, {{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BasicView',
+  components: {
+    
+  },
+  data() {
+    return {
+      title : "연세",
+    };
+  },
+  setup() {
+    
+  },
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  unmounted() {
+    
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/TestView.vue
@@ -0,0 +1,16 @@
+<template lang="Test">
+  <!-- https://velog.io/@dishate/Vscode-Vue.js-snippet-%EB%A7%8C%EB%93%A4%EA%B8%B0 -->
+  <div>
+    <p>
+      테스트
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
